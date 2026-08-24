636079c89fa125ab65b3bbe1842d01c72e6b6c98
alaidi <abdlekhaliklaidi@gmail.com>
2026-08-24 14:00:19 +0100

    SonarQube Configuration Analysis

--- user_service/src/test/java/buy01/user_service/UserServiceTest.java
+++ user_service/src/test/java/buy01/user_service/UserServiceTest.java
@@ -1,78 +1,78 @@
-// package buy01.user_service;
+package buy01.user_service;
 
-// import buy01.user_service.model.User;
-// import buy01.user_service.repo.UserRepository;
-// import buy01.user_service.producer.UserEventProducer;
-// import buy01.user_service.security.JwtUtil;
-// import buy01.user_service.exceptions.BadRequestException;
+import buy01.user_service.model.User;
+import buy01.user_service.repo.UserRepository;
+import buy01.user_service.producer.UserEventProducer;
+import buy01.user_service.security.JwtUtil;
+import buy01.user_service.exceptions.BadRequestException;
 
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-// import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-// import java.util.Map;
-// import java.util.Optional;
-// import buy01.user_service.service.UserService;
-// import buy01.user_service.service.UserBlacklistService;
-
-
-// import static org.junit.jupiter.api.Assertions.assertTrue;
-// import static org.mockito.ArgumentMatchers.any;
-// import static org.mockito.ArgumentMatchers.anyString;
-// import static org.mockito.Mockito.*;
-
-// @ExtendWith(MockitoExtension.class)
-// class UserServiceTest {
-
-//     @Mock
-//     private UserRepository userRepository;
-
-//     @Mock
-//     private PasswordEncoder passwordEncoder;
-
-//     @Mock
-//     private JwtUtil jwtUtil;
-
-//     @Mock
-//     private UserEventProducer producer;
-
-//     @Mock
-//     private UserBlacklistService blacklistService;
-
-//     @InjectMocks
-//     private UserService userService;
+import java.util.Map;
+import java.util.Optional;
+import buy01.user_service.service.UserService;
+import buy01.user_service.service.UserBlacklistService;
 
 
-//     @Test
-//     void shouldRegisterUser() {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
-//         when(userRepository.findByEmail(anyString()))
-//                 .thenReturn(Optional.empty());
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
 
-//         when(userRepository.findByUsername(anyString()))
-//                 .thenReturn(Optional.empty());
+    @Mock
+    private UserRepository userRepository;
 
-//         when(passwordEncoder.encode(anyString()))
-//                 .thenReturn("encodedPassword");
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private UserEventProducer producer;
+
+    @Mock
+    private UserBlacklistService blacklistService;
+
+    @InjectMocks
+    private UserService userService;
 
 
-//         Map<String, Object> result = userService.register(
-//                 "abdel",
-//                 "abdel@test.com",
-//                 "pass123",
-//                 "SELLER",
-//                 null
-//         );
+    @Test
+    void shouldRegisterUser() {
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.empty());
+
+        when(userRepository.findByUsername(anyString()))
+                .thenReturn(Optional.empty());
+
+        when(passwordEncoder.encode(anyString()))
+                .thenReturn("encodedPassword");
 
 
-//         assertTrue((Boolean) result.get("success"));
+        Map<String, Object> result = userService.register(
+                "abdel",
+                "abdel@test.com",
+                "pass123",
+                "SELLER",
+                null
+        );
 
-//         verify(userRepository)
-//                 .save(any(User.class));
-//     }
-// }
+
+        assertTrue((Boolean) result.get("success"));
+
+        verify(userRepository)
+                .save(any(User.class));
+    }
+}
