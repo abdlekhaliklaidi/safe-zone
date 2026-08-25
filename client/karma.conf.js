686baaa9bb205dddc66bd1f284b3a77a83acffd2
alaidi <abdlekhaliklaidi@gmail.com>
2026-08-25 10:49:38 +0100

    QUALITY GATE client

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -25,7 +25,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
       ]
     },
 
